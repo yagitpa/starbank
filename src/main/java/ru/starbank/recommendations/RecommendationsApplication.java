34c7bd97f2d148dcb34e99b8ea7ed589574c23fc
yagitpa <yakov.palchikov@gmail.com>
2026-01-13 04:50:06 +0700

    Initial commit

---
+++ src/main/java/ru/starbank/recommendations/RecommendationsApplication.java
@@ -0,0 +1,13 @@
+package ru.starbank.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationsApplication.class, args);
+    }
+
+}
