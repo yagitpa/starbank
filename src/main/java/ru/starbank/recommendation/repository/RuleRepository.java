1cdc1274b0ae882ecbf043192c35769d838c60c2
yagitpa <yakov.palchikov@gmail.com>
2026-01-15 20:21:06 +0700

    changed: - RulesDataSourceConfig.java (improve default packages path);
    added:
    - RuleRepository.java (interface for manage dynamic rules)

--- src/main/java/ru/starbank/recommendation/repository/RuleRepository.java
+++ src/main/java/ru/starbank/recommendation/repository/RuleRepository.java
@@ -1,10 +1,7 @@
 package ru.starbank.recommendation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.starbank.recommendation.domain.rules.entity.RuleEntity;
-
-import java.util.List;
 
 /**
  * Репозиторий для управления динамическими правилами рекомендаций.
@@ -13,10 +10,4 @@ import java.util.List;
  * подключён через {@code @EnableJpaRepositories} в {@code RulesDataSourceConfig}.</p>
  */
 public interface RuleRepository extends JpaRepository<RuleEntity, Long> {
-
-    /**
-     * Загружает все правила вместе с их queries одним запросом (избавляемся от N+1).
-     */
-    @Query("select distinct r from RuleEntity r left join fetch r.queries")
-    List<RuleEntity> findAllWithQueries();
 }
