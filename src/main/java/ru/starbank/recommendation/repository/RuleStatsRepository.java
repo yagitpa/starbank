a0905311c52d68b175ae048848c30718a22bcb05
yagitpa <yakov.palchikov@gmail.com>
2026-01-17 14:52:15 +0700

    Add statistic entity and DB; Increment rule statistics on rule match; Remove rule statistics when rule is deleted (on DB Delete Cascade)

--- src/main/java/ru/starbank/recommendation/repository/RuleStatsRepository.java
+++ src/main/java/ru/starbank/recommendation/repository/RuleStatsRepository.java
@@ -38,11 +38,9 @@ public interface RuleStatsRepository extends JpaRepository<RuleStatsEntity, Long
 
     /**
      * Проекция для native-запроса статистики.
-     *
-     * <p>ВАЖНО: Spring Data ожидает accessor-методы вида getXxx().</p>
      */
     interface RuleStatProjection {
-        Long ruleId();
-        Long count();
+        Long getRuleId();
+        Long getCount();
     }
 }
